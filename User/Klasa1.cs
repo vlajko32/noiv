42052d6c95a477ac01f99322748fa5fd240ba80f
Saponja <jsaponjic512@gmail.com>
2021-04-13 18:52:00 +0200

    Dodati properti Sifra, Ime i Prezime

--- User/Klasa1.cs
+++ User/Klasa1.cs
@@ -4,5 +4,9 @@ namespace User
 {
     public class Klasa1
     {
+        public int Sifra { get; set; }
+        public String Ime { get; set; }
+        public String Prezime { get; set; }
+
     }
 }
