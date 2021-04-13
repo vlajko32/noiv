ce2eb1f8a6f5ca4ba7ce686ec33f2b7f84ff259c
vlajko32 <vlajko247@gmail.com>
2021-04-13 19:10:26 +0200

    Obrisan property Prezime

--- User/Klasa1.cs
+++ User/Klasa1.cs
@@ -6,7 +6,9 @@ namespace User
     {
         public int Sifra { get; set; }
         public String Ime { get; set; }
-        public String Prezime { get; set; }
+
+        public String Opis { get; set; }
+
 
     }
 }
